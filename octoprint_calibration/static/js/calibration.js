744c170551847deef0af1a0b3eec887d29ec8da2
Stefan Gschiel <stefan.gschiel.sg@gmail.com>
2021-04-05 11:24:18 +0200

    implemented front end for show e step calibrations view

--- octoprint_calibration/static/js/calibration.js
+++ octoprint_calibration/static/js/calibration.js
@@ -40,6 +40,85 @@ $(function() {
         };
     };
 
+    // https://jsfiddle.net/AnkUser/975ncawv/149/
+    // https://stackoverflow.com/questions/51638095/knockout-table-paging
+    function EStepsCalibrationModel(creationDate, filamentName, filamentType, hotendTemp, oldEsteps, newESteps) {
+        var self = this;
+        self.creationDate = ko.observable(creationDate);
+        self.filamentName = ko.observable(filamentName);
+        self.filamentType = ko.observable(filamentType);
+        self.hotendTemp = ko.observable(hotendTemp);
+        self.oldEsteps = ko.observable(oldEsteps);
+        self.newESteps = ko.observable(newESteps);        
+    }
+    EStepsCalibrationModel.fromRawDataPoint = function(dto) {
+        return new EStepsCalibrationModel(dto.creationDate, dto.filamentName, dto.filamentType, dto.hotendTemp, dto.oldEsteps, dto.newESteps);
+    }
+
+    function ShowEStepsCalibrationsView(entriesPerPage) {
+        var self = this;
+
+        // Start of logic for Paging    
+  
+        self.items = ko.observableArray();
+        this.all =  self.items;
+
+        self.pageNumber = ko.observable(0);
+        self.nbPerPage = entriesPerPage;
+    
+        this.totalPages = ko.computed(function() {
+            var div = Math.floor(self.all().length / self.nbPerPage);
+            div += self.all().length % self.nbPerPage > 0 ? 1 : 0;
+            return div - 1;
+        });
+
+        this.paginated = ko.computed(function() {            
+            var first = self.pageNumber() * self.nbPerPage;
+            console.log("paginated called. first=" + first);
+            console.log("self.all=" + JSON.stringify(self.all));
+            console.log("self.items=" + JSON.stringify(self.items));
+            //return self.all.slice(first, first + self.nbPerPage);
+            return self.items.slice(first, first + self.nbPerPage);
+        });
+    
+        this.hasPrevious = ko.computed(function() {
+            return self.pageNumber() !== 0;
+        });
+  
+        this.hasNext = ko.computed(function() {
+            return self.pageNumber() !== self.totalPages();
+        });
+  
+        this.next = function() {
+            if(self.pageNumber() < self.totalPages()) {
+                self.pageNumber(self.pageNumber() + 1);
+            }
+        }
+  
+        this.previous = function() {
+            if(self.pageNumber() != 0) {
+                self.pageNumber(self.pageNumber() - 1);
+            }
+        }  
+
+
+        // End of Logic for Paging
+
+        self.loadData = function(rawData) {
+            self.items(rawData.map(EStepsCalibrationModel.fromRawDataPoint))
+        }
+
+        self.select = function(item) {
+            if (item === self.selected()) {
+                self.selected(null);
+            } else {
+                self.selected(item);
+            }   
+        };
+
+        self.selected = ko.observable(self.items()[0]);
+    }
+
     // http://jsfiddle.net/rniemeyer/SSY6n/
     function Step(id, name, template, model) {
         var self = this;
@@ -203,11 +282,86 @@ $(function() {
                 eStepCalibFinished: function() {
                     self.parent.goToStartPage();
                 }
+            }),
+            // special step for show E Steps calibration view
+            new Step(6, "ShowEStepCalibrationsView", "eSteps_showCalibrationsTmpl", {
+                eStepsCalibrationView: undefined,
+                initialize: function() {
+                    var innerSelf = this;
+                    var entriesPerPage = 2;
+                    innerSelf.eStepsCalibrationView = new ShowEStepsCalibrationsView(entriesPerPage);
+                },
+                loadData: function(rawData) {
+                    var innerSelf = this;
+                    console.log("Loading data: " + JSON.stringify(rawData))
+                    innerSelf.eStepsCalibrationView.loadData(rawData);
+                }                
             })
         ]);
 
         self.firstStep = function() {
             return self.stepModels()[0];
+        }
+
+        self.stepModels()[6].model().initialize();
+        self.showEStepsCalibrations = function() {
+            // load data from backend - for testing use dummy data
+            var dummyData = [
+                {
+                    creationDate: "2021-01-01 12:24:25",
+                    filamentName: "TestFilament1", 
+                    filamentType: "PLA", 
+                    hotendTemp: 210, 
+                    oldEsteps: 90.0, 
+                    newESteps: 91.5
+                },
+                {
+                    creationDate: "2021-02-01 12:24:25",
+                    filamentName: "TestFilament2", 
+                    filamentType: "PETG", 
+                    hotendTemp: 230, 
+                    oldEsteps: 91, 
+                    newESteps: 92.33
+                },
+                {
+                    creationDate: "2021-03-01 12:24:25",
+                    filamentName: "TestFilament3", 
+                    filamentType: "ABS", 
+                    hotendTemp: 250, 
+                    oldEsteps: 92, 
+                    newESteps: 91.3
+                },
+                {
+                    creationDate: "2021-04-01 12:24:25",
+                    filamentName: "TestFilament4", 
+                    filamentType: "TPU", 
+                    hotendTemp: 230, 
+                    oldEsteps: 90.0, 
+                    newESteps: 95
+                },
+                {
+                    creationDate: "2021-04-02 12:24:25",
+                    filamentName: "TestFilament5", 
+                    filamentType: "PLA", 
+                    hotendTemp: 200, 
+                    oldEsteps: 91, 
+                    newESteps: 90.5
+                },
+                {
+                    creationDate: "2021-04-03 12:24:25",
+                    filamentName: "TestFilament6", 
+                    filamentType: "PETG", 
+                    hotendTemp: 230, 
+                    oldEsteps: 90.5, 
+                    newESteps: 93
+                }
+            ];
+
+            // and call <Step>.loadData(loadedDataInDto)
+            showCalibStep = self.stepModels()[6];
+            showCalibStep.model().loadData(dummyData);
+
+            return showCalibStep;
         }
     }
 
@@ -230,6 +384,10 @@ $(function() {
                 newEStepsCalibration: 
                     function() {
                         self.currentStep(self.eStepsCalibrationTool.firstStep());
+                    },
+                showEStepsCalibrations:
+                    function() {
+                        self.currentStep(self.eStepsCalibrationTool.showEStepsCalibrations());
                     }
             }),
             new Step(1, "ErrorPage", "calibPlugin_errorPageTmpl", {
